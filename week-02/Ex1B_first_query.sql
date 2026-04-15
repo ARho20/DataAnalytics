812fbed0baa7d73d9e8aa7ced8a54635295d8723
aspenne rhoden <arhoden@my.yearupunited.org>
2026-04-15 10:38:11 -0400

    Added queries of actorand film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -1,3 +1,7 @@
 /*
 Sakila is a database used to collect information about actors and films for use by a movie rental company
-/*
+*/
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records
