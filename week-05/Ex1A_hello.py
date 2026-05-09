92146fb18f927d62343be3157c6fbcae79229995
Arho20 <Arhoden.r20@gmail.com>
2026-05-09 17:57:04 -0400

    Ex1A finished

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,3 +1,22 @@
 print('Hello World!')
 
-clear
+message = 'Hello World!' 
+print(message)
+
+# Hello world prints twice because there are two print statements
+
+
+#Displaying dollars and cents
+
+dollars = 3
+cents = .50
+print(dollars + cents)
+#The result came back as 3.5 it dropped the zero
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print (d_str + " " + c_str)
+
+
