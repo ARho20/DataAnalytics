67ff3913a7da93c5731c039430c826c88befd389
Arho20 <Arhoden.r20@gmail.com>
2026-05-26 19:47:53 -0400

    Added week 5 exercise changes

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,4 +1,4 @@
-print('Hello World!')
+=print('Hello World!')
 
 message = 'Hello World!' 
 print(message)
---
+++ week-05/Ex1B_variable_examples.py
@@ -0,0 +1,46 @@
+customer_id = 876 # I created this ID in numeric because Id's are usualy numbers
+customer_name = 'Tommy' # I used a string because names are text which is a string
+customer_gender = 'male' # I used a string because gender are text which is a string
+drivers_ln = '9491733817' # I used a string because license numbers are treated as text
+auto_policy_n = '12345678' # I created this variable for auto policy number but it may not be readable since its just letter abreviations
+
+aspenne_rho = "Aspenne Rhoden"
+queens_ny = "Queens, NY"
+
+# Python reserved words:
+# False
+#^None^
+# True
+# and
+# ^as^
+# ^assert^
+# ^async^
+# ^await^
+# ^break^
+# ^class^
+# continue
+# ^def^
+# del : This is used to delete from the memory
+# elif : stands for else if to check if false
+# else
+# except
+# finally
+# ^for^
+# ^from^
+# ^global^
+# if : this is to check if it is true
+# ^import^
+# ^in^
+# is
+# ^lambda^
+# ^nonlocal^
+# ^not^
+# or : Used when atleast one condition needs to be true
+# ^pass^
+# ^raise^
+# ^return^
+# ^try^
+# while : this is to repeat the code as long as it is true
+# ^with^
+# ^yield^
+
