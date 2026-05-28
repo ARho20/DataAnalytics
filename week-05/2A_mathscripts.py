2d4502c27cdf65551f5cf4fa297a078b7771d7c6
Arho20 <Arhoden.r20@gmail.com>
2026-05-28 01:23:15 -0400

    area of rectangle

---
+++ week-05/2A_mathscripts.py
@@ -0,0 +1,22 @@
+# Assets 
+cash = 5000
+car_value = 8000
+
+# Debts 
+credit_card = 2000
+loan = 3000
+
+# Totals
+total_assets = cash + car_value
+total_debts = credit_card + loan
+
+#Net work
+net_worth = total_assets - total_debts
+
+#Output
+print ("Your total assets are " + str(total_assets))
+print ("Your total debts are " + str(total_debts))
+print ("Your net worth is " + str(net_worth))
+
+
+
---
+++ week-05/area_of_rectangle.py
@@ -0,0 +1,10 @@
+side_a = 2
+side_b = 20
+
+area = side_a * side_b
+
+print("Side A is " + str(side_a))
+print("Side B is " + str(side_b))
+print("The area of the rectangle is " + str(area))
+
+
