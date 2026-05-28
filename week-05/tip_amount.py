af2202d31d8049d6e0e2eb6a7966df8762829a70
Arho20 <Arhoden.r20@gmail.com>
2026-05-28 14:30:50 -0400

    tip amount

---
+++ week-05/tip_amount.py
@@ -0,0 +1,6 @@
+restaurant_bill = 75
+tip_percentage = 0.20
+
+tip_amount = restaurant_bill * tip_percentage
+
+print("The tip on a $" + str(restaurant_bill) + " restatuarant bill is $" + str(tip_amount))
